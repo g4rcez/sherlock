42126b67919193924224c8019ffc933c0e8e5e13
Allan Garcez <allan.f.garcez@gmail.com>
2017-08-18 00:43:32 -0300

    Refix install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 if [[ "$(whoami)" == "root" ]]; then
-	cd /tmp
 	defaultDir="$(pwd)"
+	cd /tmp
 
 	# Em alguns ambientes o wget padrão é bloqueado, com uma pequena definição de
 	# user-agent, isso é consertado. xD
 
-	wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz --user-agent Firefox| tar -xf
+	wget 'https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz' --user-agent Firefox
+	tar -xf 'Python-3.6.2.tar.xz'
 	cd Python-3.6.2/
 	sudo ./configure && sudo make && sudo make install
 
 	# Mesma coisa citada anteriormente
 	cd /tmp && wget "https://bootstrap.pypa.io/get-pip.py" --user-agent 'Firefox HueBr' -v
+	sudo python3.6 get-pip.py
 
-	sudo python3 get-pip.py
 	cd "$defaultDir"
 	sudo pip install -r requirements.txt
 else
